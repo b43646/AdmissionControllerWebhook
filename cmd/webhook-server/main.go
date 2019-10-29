c02dffdbf8e5e45076b375165f0805bd998a214e
loren <408514942@qq.com>
2019-10-29 10:23:51 +0800

    change uid

--- cmd/webhook-server/main.go
+++ cmd/webhook-server/main.go
@@ -85,7 +85,7 @@ func applySecurityDefaults(req *v1beta1.AdmissionRequest) ([]patchOperation, err
 			patches = append(patches, patchOperation{
 				Op:    "add",
 				Path:  "/spec/securityContext/runAsUser",
-				Value: 1234,
+				Value: 1000100000,
 			})
 		}
 	} else if *runAsNonRoot == true && (runAsUser != nil && *runAsUser == 0) {
